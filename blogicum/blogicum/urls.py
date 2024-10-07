e775f6b52c8e18c6a6a2ce306e0f22535d2f2313
Dmitriev Kirill 'KirodGK <KirillDm2001@yandex.ru>
2024-10-07 23:28:08 +0300

    first review

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -19,7 +19,6 @@ urlpatterns = [
     ),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('blog.urls', namespace='blog')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.internal_server_error500'
-
